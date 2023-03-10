d550571ad2683e3a492b1a062cba56b61598ccde
fr-daria <frantsuzova00000@yandex.ru>
2023-03-10 11:19:02 +0300

    Изменения, форматирование и удаление ненужного

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,17 +1,16 @@
 import java.util.Random;
 
 public class MagicBox<T> {
-    protected T item;
+
     protected T[] items;
 
-    public MagicBox( int size) {
-        this.item = item;
+    public MagicBox(int size) {
         T[] items = (T[]) new Object[size];
     }
 
     public boolean add(T item) {
-        for (int i = 0 ; i < items.length ; i++) {
-            if (items[i] != null) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
                 items[i] = item;
                 return true;
             }
@@ -21,15 +20,14 @@ public class MagicBox<T> {
 
     public <T> T pick() {
         //try {
-            for (int i = 0; i < items.length; i++){
-                if (items[i] == null){
-                    throw new RuntimeException("коробка не полна и осталось ещё " + " ячеек заполнить");
-                }
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("коробка не полна и осталось ещё " + " ячеек заполнить");
             }
-        //} catch (RuntimeException e) {
-           // System.out.println("коробка не полна и осталось ещё " + " ячеек заполнить");}
+        }
+
         Random random = new Random();
-        int randomInt = random.nextInt(items.length); // джава подберёт случайное число от 0 до ЧИСЛО невключительно
+        int randomInt = random.nextInt(items.length);
 
         return (T) items[randomInt];
     }
